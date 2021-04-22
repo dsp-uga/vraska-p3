adddbc4feae336d241a9ed61a059f3bd5e3a0dd5
nathanWynn <nww15119@uga.edu>
2021-04-22 11:29:52 -0400

    initial model using tensorflow unet, work in progress.

---
+++ src/models/train_model.py
@@ -0,0 +1,128 @@
+import os
+import numpy as numpy
+import tarfile
+import cv2
+import tensorflow as tf
+#from tensorflow_examples.models.pix2pix import pix2pix
+import tensorflow_datasets as tfds
+#from IPython.display import clear_output
+#import matplotlib.pyplot as plt
+
+data_path = 'data/raw/'
+data_processed = 'data/processed/'
+#train = []
+#with open(data_path + 'train.txt') as f:
+#    for line in f:
+#        train.append(line.strip())
+
+#for file in train:
+#    tar = tarfile.open(data_path + 'data/' + file + '.tar')
+#    tar.extractall(path=data_processed)
+
+TRAIN_LENGTH = 325
+BATCH_SIZE = 64
+BUFFER_SIZE = 1000
+STEPS_PER_EPOCH = TRAIN_LENGTH // BATCH_SIZE
+OUTPUT_CHANNELS = 3
+
+builder = tfds.ImageFolder('data/processed')
+print(builder.info)
+ds = builder.as_dataset()
+
+
+train = ds['data'].map(load_image_train, num_parallel_calls=tf.data.AUTOTUNE)
+test = ds['data'].map(load_image_test)
+
+train_dataset = train.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+train_dataset = train_dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
+test_dataset = test.batch(BATCH_SIZE)
+
+base_model = tf.keras.applications.MobileNetV2(input_shape=[128, 128, 3], include_top=False)
+
+# Use the activations of these layers
+layer_names = [
+    'block_1_expand_relu',   # 64x64
+    'block_3_expand_relu',   # 32x32
+    'block_6_expand_relu',   # 16x16
+    'block_13_expand_relu',  # 8x8
+    'block_16_project',      # 4x4
+]
+base_model_outputs = [base_model.get_layer(name).output for name in layer_names]
+
+# Create the feature extraction model
+down_stack = tf.keras.Model(inputs=base_model.input, outputs=base_model_outputs)
+
+down_stack.trainable = False
+
+model = unet_model(OUTPUT_CHANNELS)
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+EPOCHS = 20
+model_history = model.fit(train_dataset, epochs=EPOCHS,
+                          steps_per_epoch=STEPS_PER_EPOCH)
+
+def create_mask(pred_mask):
+  pred_mask = tf.argmax(pred_mask, axis=-1)
+  pred_mask = pred_mask[..., tf.newaxis]
+  return pred_mask[0]
+
+
+def show_predictions(dataset=None, num=1):
+  if dataset:
+    for image, mask in dataset.take(num):
+      pred_mask = model.predict(image)
+      display([image[0], mask[0], create_mask(pred_mask)])
+  else:
+    display([sample_image, sample_mask,
+             create_mask(model.predict(sample_image[tf.newaxis, ...]))])
+
+def unet_model(output_channels):
+  inputs = tf.keras.layers.Input(shape=[128, 128, 3])
+
+  # Downsampling through the model
+  skips = down_stack(inputs)
+  x = skips[-1]
+  skips = reversed(skips[:-1])
+
+  # Upsampling and establishing the skip connections
+  for up, skip in zip(up_stack, skips):
+    x = up(x)
+    concat = tf.keras.layers.Concatenate()
+    x = concat([x, skip])
+
+  # This is the last layer of the model
+  last = tf.keras.layers.Conv2DTranspose(
+      output_channels, 3, strides=2,
+      padding='same')  #64x64 -> 128x128
+
+  x = last(x)
+
+  return tf.keras.Model(inputs=inputs, outputs=x)
+
+def normalize(input_image, input_mask):
+  input_image = tf.cast(input_image, tf.float32) / 255.0
+  input_mask -= 1
+  return input_image, input_mask
+
+@tf.function
+def load_image_train(datapoint):
+  input_image = tf.image.resize(datapoint['image'], (128, 128))
+  input_mask = tf.image.resize(datapoint['segmentation_mask'], (128, 128))
+
+  if tf.random.uniform(()) > 0.5:
+    input_image = tf.image.flip_left_right(input_image)
+    input_mask = tf.image.flip_left_right(input_mask)
+
+  input_image, input_mask = normalize(input_image, input_mask)
+
+  return input_image, input_mask
+
+def load_image_test(datapoint):
+  input_image = tf.image.resize(datapoint['image'], (128, 128))
+  input_mask = tf.image.resize(datapoint['segmentation_mask'], (128, 128))
+
+  input_image, input_mask = normalize(input_image, input_mask)
+
+  return input_image, input_mask
